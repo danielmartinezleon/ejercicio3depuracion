a4a24906b12e7a642624778a0a7c2350a8dd9b52
Daniel Martínez León <daniel1martinezleon@gmail.com>
2023-11-30 14:28:33 +0100

    Ejercicio resuelto

---
+++ src/ejercicio/Ejercicio3.java
@@ -0,0 +1,25 @@
+package ejercicio;
+
+import java.util.Arrays; 
+
+public class Ejercicio3 {
+    
+    public static void main(String[] args) {
+        
+        int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        
+        // Insert an element in 3rd position of the array (index->2, value->5)
+        
+        int Index_position = 2;
+        int newValue = 35;
+        
+        System.out.println("Original Array : "+Arrays.toString(my_array));     
+        
+        for(int i=0; i < my_array.length; i++){
+            my_array[i] = my_array[i++];
+            my_array[Index_position] = newValue;
+        }
+        
+        System.out.println("New Array: "+Arrays.toString(my_array));
+    }
+}
